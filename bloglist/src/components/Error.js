0314f8d00363dd5617aee61aa74ebbcae5f619c3
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2023-06-26 12:12:54 +0300

    7.9: koodin automaattinen muotoilu, complete

---
+++ bloglist/src/components/Error.js
@@ -0,0 +1,9 @@
+const Error = (props) => {
+  if (!props.message) {
+    return <div className="empty"></div>
+  } else {
+    return <div className="error">{props.message}</div>
+  }
+}
+
+export default Error
